e850903b678504c8d27fda35a488bd2c697417ab
Maxim Tkachev <maximtkachev99@gmail.com>
2021-06-29 01:52:14 +0300

    trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,25 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  const arr = string.split('');
+  const newArr = [];
 
+  let char = arr[0];
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== char) {
+      char = arr[i];
+      count = 0;
+    }
+
+    if (count === size) {
+      continue;
+    }
+
+    newArr.push(arr[i]);
+    count++;
+  }
+
+  return newArr.join('');
 }
