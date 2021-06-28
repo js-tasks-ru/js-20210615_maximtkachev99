c8bb74b0b62fbcc7a3e3ab5f6ed8dba1623479bd
Maxim Tkachev <maximtkachev99@gmail.com>
2021-06-29 01:25:36 +0300

    createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathArr = path.split('.');
+  let index = 0;
 
+  const getter = function (obj) {
+    if (index === pathArr.length - 1 || obj[pathArr[index]] === undefined) {
+      return obj[pathArr[index]];
+    } else {
+      index++;
+      return getter(obj[pathArr[index - 1]]);
+    }
+  };
+
+  return getter;
 }
